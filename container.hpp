baad5dd42cb1b2150301e8b068274b7218c4848f
Vadim <varem99@mail.ru>
2020-12-26 23:51:15 +0300

    template and add element function

--- container.hpp
+++ container.hpp
@@ -1,47 +1,123 @@
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 namespace cnt {
 
     typedef int8_t data_t;
     static const char* filename = "buffer.txt";
     enum { SIZE = 1 << 12};
 
+    template<typename T>
     class Container {
     public:
         Container() = delete;
-
+        explicit Container(uint32_t size);
         ~Container();
 
-        explicit Container(uint32_t size);
+        T& operator[](uint32_t idx) = delete;
+        void addElem(uint32_t idx, T elem);
+        void save_to_file();
+        void write_to_file(uint32_t pos);
+        // uint32_t calculate_position(uint32_t pos);
 
     private:
-        uint32_t size_;
-        data_t *data_;
+        uint32_t size_; // limit of RAM used
+        uint32_t pos_;  // overall position (disk + RAM)
+        int32_t offset_; // RAM position
+        T *data_;
         FILE* fout_;
     };
 
-    Container::~Container() {
+    template<typename T>
+    Container<T>::~Container() {
         size_ = 0;
         if(fclose(fout_))
             std::cerr << "Close failed!\n";
         std::cout << "container destructed\n";
     }
 
-    Container::Container(uint32_t size) :
+    template<typename T>
+    Container<T>::Container(uint32_t size) :
             size_(size),
+            pos_(0),
+            offset_(-1),
             data_(nullptr),
             fout_(nullptr)
     {
         std::cout << "Container constructed with size: " << size_ << std::endl;
-        fout_ = fopen(filename, "rw");
+        fout_ = fopen(filename, "r+w");
         if(fout_ == nullptr)
         {
             std::cerr << "Open failed!\n";
         }
         try {
-            data_ = new data_t[SIZE];
+            data_ = new T[size_];
         } catch (const std::bad_alloc& e) {
             std::cerr << e.what() << std::endl;
         }
     }
+
+    template<typename T>
+    void Container<T>::save_to_file()
+    {
+        fwrite(data_, sizeof(T), size_, fout_);
+        if(ferror(fout_))
+        {
+            std::cerr << "Error writing to file\n";
+        }
+    }
+
+    template<typename T>
+    void Container<T>::write_to_file(uint32_t pos)
+    {
+        fwrite(data_, sizeof(T), size_, fout_);
+        if(ferror(fout_))
+        {
+            std::cerr << "Error writing to file\n";
+        }
+    }
+
+    template<typename T>
+    void Container<T>::addElem(uint32_t idx, T elem)
+    {
+        std::cout << "idx: " << idx << " offset: " << offset_ << std::endl;
+        if(idx < size_)
+        {
+            std::cout << "idx is not out of range\n";
+            data_[idx] = elem;
+            offset_++;
+            pos_++;
+        }
+        else
+        {
+            if((idx == size_) && (offset_ == size_ - 1)) //
+            {
+                std::cout << "fulfilled for the first time" << std::endl;
+                save_to_file();
+                offset_  = 0;
+                pos_     = idx;
+                data_[0] = elem;
+            }
+            else
+            {
+                std::cout << "idx > size, idx: " << idx << std::endl;
+                data_[idx % size_] = elem;
+            }
+        }
+        // return data_[idx];
+
+    }
+
+/*    template<typename T>
+    uint32_t Container<T>::calculate_position(uint32_t pos)
+    {
+        if(pos < pos_)
+            return pos;
+        else
+        {
+
+        }
+    }*/
+
+
 }
--- main.cpp
+++ main.cpp
@@ -10,12 +10,34 @@ int main()
     return 0;
 }
 
+/*
+ *  // create objects with max RAM storage
+    Container<int8_t> string(1024);
+    Container<float> array(4096);
+    // fill objects
+    for(int i = 0; i < 10000; ++i)
+    {
+        string[i] = i;
+        array[i]  = i;
+    }
+    // get some data
+    float middle = array[5000];
+ */
+
 void test_container()
 {
-    auto cont = new cnt::Container(128);
-    cnt::Container container(256);
-    for(int i = 0; i < 10; ++i)
+    auto cont = new cnt::Container<int>(10);
+    cnt::Container<uint32_t> container(10);
+    for(uint32_t i = 0; i < 10; ++i)
     {
-        cnt::Container c(256);
+        container.addElem(i, i);
     }
+    /*for(int i = 0; i < 10; ++i)
+    {
+        std::cout << container[i] << " ";
+    }*/
+    std::cout << std::endl;
+
+    container.addElem(10, 10);
+    container.addElem(11, 11);
 }
